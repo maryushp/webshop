d2e5db86774c2b908a2a12fd69c82b94b347a491
MfalconP <maryushp@gmail.com>
2023-09-26 19:01:10 +0200

    feat (dev_spring) added one more custom exception

---
+++ src/main/java/com/project/exceptionhandler/exceptions/InvalidElementException.java
@@ -0,0 +1,8 @@
+package com.project.exceptionhandler.exceptions;
+
+public class InvalidElementException extends ApplicationException {
+
+    public InvalidElementException(String message) {
+        super(message);
+    }
+}
