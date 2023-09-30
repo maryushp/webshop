a4a0e1a431a9416e134245ae377e0a3dd803117a
MfalconP <maryushp@gmail.com>
2023-09-30 14:11:46 +0200

    feat (dev_spring) added CategoryRepositoryTest class

---
+++ src/test/java/com/project/repository/CategoryRepositoryTest.java
@@ -0,0 +1,86 @@
+package com.project.repository;
+
+import com.project.model.Category;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryRepositoryTest {
+    private CategoryRepository categoryRepository;
+    private EmbeddedDatabase embeddedDatabase;
+
+    @BeforeEach
+    public void init() {
+        this.embeddedDatabase = new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript("database/Structure.sql")
+                .addScript("database/Data.sql")
+                .build();
+        this.categoryRepository = new CategoryRepository(new JdbcTemplate(embeddedDatabase));
+    }
+
+    @Test
+    void selectAllSuccess() {
+        Category cat1 = new Category(1, "Category1");
+        Category cat2 = new Category(2, "Category2");
+        assertEquals(List.of(cat1, cat2), categoryRepository.selectAll());
+    }
+
+    @Test
+    void selectDependenciesById() {
+        assertEquals(new ArrayList<>(), categoryRepository.selectDependenciesById(0));
+    }
+
+    @Test
+    void selectCategoryById() {
+        Category category = new Category(1, "Category1");
+        assertEquals(category, categoryRepository.selectById(1).orElseThrow());
+    }
+
+    @Test
+    void insertCategory() {
+        Category cat = new Category();
+        cat.setName("TEST_CATEGORY");
+        assertTrue(categoryRepository.insert(cat));
+        int id = categoryRepository.getId(cat).orElseThrow();
+        assertEquals(cat.getName(), categoryRepository.selectById(id).orElseThrow().getName());
+    }
+
+    @Test
+    void updateCategory() {
+        Map<String, String> objectMap = new HashMap<>();
+        objectMap.put("name", "TEST");
+        assertEquals("TEST", categoryRepository.update(objectMap, 1).orElseThrow().getName());
+    }
+
+    @Test
+    void deleteCategory() {
+        Category cat = categoryRepository.selectById(1).orElseThrow();
+        categoryRepository.delete(1);
+        assertFalse(categoryRepository.isExists(cat));
+    }
+
+    @Test
+    void isExistsCategory() {
+        Category cat = categoryRepository.selectById(1).orElseThrow();
+        assertTrue(categoryRepository.isExists(cat));
+    }
+
+    @AfterEach
+    public void finish() {
+        embeddedDatabase.shutdown();
+    }
+
+
+}
