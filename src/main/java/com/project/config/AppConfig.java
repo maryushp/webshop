8fdec4125a3ec9fdf8beeaa3933a887ed555b582
MfalconP <maryushp@gmail.com>
2023-10-01 16:45:26 +0200

    feat (dev_spring) added swagger API documentation

--- src/main/java/com/project/config/AppConfig.java
+++ src/main/java/com/project/config/AppConfig.java
@@ -10,15 +10,19 @@ import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import javax.sql.DataSource;
 
 @Configuration
+@EnableSwagger2
 @ComponentScan("com.project")
 @PropertySource("classpath:database.properties")
 @EnableWebMvc
 @RequiredArgsConstructor
-public class AppConfig {
+public class AppConfig implements WebMvcConfigurer {
     private final Environment environment;
     private final ApplicationContext applicationContext;
 
@@ -30,6 +34,15 @@ public class AppConfig {
         dataSource.setUsername(environment.getProperty("user"));
         dataSource.setPassword(environment.getProperty("password"));
         return dataSource;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 
     @Bean
