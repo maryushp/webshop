a510a83bdc49805a60e773f54483721269b88421
MfalconP <maryushp@gmail.com>
2023-11-15 16:55:50 +0100

    fix (dev_spring_boot) refactored auth controller

--- src/main/java/com/project/auth/controller/AuthenticationController.java
+++ src/main/java/com/project/auth/controller/AuthenticationController.java
@@ -14,12 +14,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import static com.project.utils.exceptionhandler.ExceptionMessages.*;
 
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
     private final AuthenticationService authService;
+    public static final String AUTHORIZATION = "Authorization";
 
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(@RequestBody @Valid RegisterRequest request) {
@@ -34,13 +36,13 @@ public class AuthenticationController {
     @PostMapping("/refresh-token")
     public ResponseEntity<Void> refreshToken(HttpServletRequest request, HttpServletResponse response) {
         final String jwt;
-        String bearerToken = request.getHeader("Authorization");
+        String bearerToken = request.getHeader(AUTHORIZATION);
         if (bearerToken == null || !bearerToken.startsWith("Bearer ")) {
-            throw new InvalidTokenException("Refresh request should contain refresh token.");
+            throw new InvalidTokenException(REQUEST_SHOULD_CONTAIN_TOKEN);
         }
         jwt = bearerToken.substring(7);
         String token = authService.refreshToken(jwt);
-        response.setHeader("Authorization",
+        response.setHeader(AUTHORIZATION,
                 "Bearer " + token);
         return ResponseEntity.noContent().build();
     }
