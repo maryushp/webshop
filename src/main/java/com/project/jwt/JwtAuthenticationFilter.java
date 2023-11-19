81f123395d5a7779ec8b19ff75ccf3d8ecbc2f34
MfalconP <maryushp@gmail.com>
2023-11-19 21:58:41 +0100

    feat (dev_spring_boot) added jwt service interface

--- src/main/java/com/project/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/project/jwt/JwtAuthenticationFilter.java
@@ -24,7 +24,7 @@ import java.io.IOException;
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
-    private final JwtService jwtService;
+    private final JwtServiceImpl jwtService;
     private final UserDetailsService userDetailService;
 
     @Override
